de65103074b93c3786e07da3fe0e7bb9aa8a58de
omar <omar@sahab.ma>
2023-12-03 20:02:44 +0000

    rebuild

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701633208';
+const cacheName = 'chirpy-1701633741';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
