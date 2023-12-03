acf1085cf182d8ab853a905caa117c02aae77e0d
omar <omar@sahab.ma>
2023-12-03 20:05:05 +0000

    update authors

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701633741';
+const cacheName = 'chirpy-1701633891';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
