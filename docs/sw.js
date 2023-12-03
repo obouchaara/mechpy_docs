88228efaece68e80d6cfb219811a1b0d38d5a546
omar <omar@sahab.ma>
2023-12-03 20:07:38 +0000

    omar url

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701633891';
+const cacheName = 'chirpy-1701634023';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
