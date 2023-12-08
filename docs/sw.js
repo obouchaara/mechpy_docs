9c9ee043d0be645c4503e1dc64779665c533f5cc
omar <omar@sahab.ma>
2023-12-08 10:44:21 +0000

    mathjax with https

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1702031981';
+const cacheName = 'chirpy-1702032226';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
